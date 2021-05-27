fc1810aad7da15ac67e624e04c2710209bf29cc7
paco <afruiz95@hotmail.com>
2021-05-27 10:48:09 +0200

    second commit

--- src/paquete1/Main.java
+++ src/paquete1/Main.java
@@ -3,6 +3,9 @@ package paquete1;
 public class Main {
 
     //aqui realizare cambios para sincronizar el git
+    private String nombre ="Francisco Ruiz Ortega";
+    private int nota_ENDES = 10;
+    //con eso nos aseguramos que hay cambios y tal
 
 
 }
